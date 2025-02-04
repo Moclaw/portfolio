339950bf1e2d05cdf41603d9a1eef9c0b6fb20f0
moclaw <mocduonglam86@gmail.com>
2025-02-04 23:47:39 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,6 @@ export default defineConfig({
 		host: true, // needed for the Docker Container port mapping to work
 		strictPort: true,
 		port: 5300, // you can replace this port with any port
+		allowedHosts: ['moclawr.com']
 	  }
 })
